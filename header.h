04d7fecd9c0d4d3409265cdf76eea75e3f9e943e
dekuu5 <deku332211@gmail.com>
2023-04-28 16:17:18 +0300

    functions countdigits getmax getdigit are finsih and working

--- header.h
+++ header.h
@@ -5,4 +5,14 @@
 #ifndef RADIXSORTING_HEADER_H
 #define RADIXSORTING_HEADER_H
 
+
+
+
+
+// radix.cpp
+
+int getDigit(int val,int digit);
+int getMaxNumber(int *arr, int count);
+int countDigits(int val);
+
 #endif //RADIXSORTING_HEADER_H
--- radix.cpp
+++ radix.cpp
@@ -1,3 +1,20 @@
-//
-// Created by pc on 4/28/2023.
-//
+#include <math.h>
+#include <stdio.h>
+#include "header.h"
+#define Ten 10
+//getMax();
+//CountDigits()
+
+int getMaxNumber(int *arr, int count){
+    int max = arr[0];
+    for (int i = 0; i < count; ++i) {
+        if(arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+int countDigits(int val){
+    return (val==0)?1:log10(val)+1;
+}
+int getDigit(int val,int digit) {
+    return (int)(val / pow(Ten, digit - 1)) % Ten;
+}
